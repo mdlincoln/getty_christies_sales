a69def6dea629c01779a194ba1ce59fe07896527
mdlincoln <matthew.d.lincoln@gmail.com>
2014-03-17 11:22:16 -0400

    Add a "period" factor to sales, and plot by_month faceted by decade

--- R/sales_analysis.R
+++ R/sales_analysis.R
@@ -8,7 +8,7 @@ library(lubridate)
 # Correct column headers
 correct_cols <- c("Catalogue No.","Lot Number","Artist Name 1","Artist Name 2","Artist Name 3","Artist Name 4","Artist Name 5","Title","Annotations","Object Type","Object Notes","Inscription","Seller","Transaction","Buyer","Lot Notes","Previous Owner","Previous Sales","Post Owner","Post Sales","Sale Date","Expert","Commissaire Pr.","Auction House","Sale Location","Lugt Number")
 
-dutch_paintings <- read.csv(
+base_dutch_paintings <- read.csv(
   "../data/dutch_paintings.csv",
   skip=1,
   header=FALSE,
@@ -17,11 +17,28 @@ dutch_paintings <- read.csv(
   stringsAsFactors=FALSE
 )
 
-#### Generate year column ####
+dutch_paintings <- base_dutch_paintings
+
+#### Generate date columns ####
 date_pattern <- "^(.{11})"
 dutch_paintings$Date <- ymd(str_extract(dutch_paintings$Sale.Date, date_pattern))
 dutch_paintings$Year <- as.integer(year(dutch_paintings$Date))
 dutch_paintings$Month <- as.factor(month(dutch_paintings$Date))
+
+# Periods
+
+period <- 1770
+while(period<1840) {
+  start <- period
+  end <- period + 10
+  per_name <- paste(start,"-",end)
+  print(per_name)
+  dutch_paintings$Period[dutch_paintings$Year>=start & dutch_paintings$Year<end] <- per_name
+  period <- end
+}
+
+dutch_paintings$Period <- as.factor(dutch_paintings$Period)
+ggplot(dutch_paintings, aes(x=Month)) + geom_bar(stat="bin") + facet_wrap(~ Period)
 
 #### Group and plot by year and month ####
 by_year <- dutch_paintings %.% group_by(Year) %.% summarize(count=n())
